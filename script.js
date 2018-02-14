0e82610858b55fd1bd1dc50963492d3bc49b2e46
endnicely <zhoujie66@yahoo.com>
2018-02-13 20:37:23 -0800

    Lab04 - Invoice

---
+++ script.js
@@ -0,0 +1,42 @@
+/*eslint-env browser*/
+//Global Variable
+var customerType;
+var invoiceTotal;
+var discountPercent;
+var discountAmount;
+var newInvoiceTotal;
+
+//Display Message
+window.document.write("<h3>Welcome to the Invoice Application</h3>");
+//Ask 
+customerType = window.prompt("Enter customer type(r/w)");
+invoiceTotal = parseFloat(window.prompt("Enter invoice total"));
+
+if (customerType === "r") {
+    if (invoiceTotal > 0 && invoiceTotal < 100) {
+        discountPercent = 0;
+    } else if (invoiceTotal >= 100 && invoiceTotal < 250) {
+        discountPercent = 0.1;             
+    } else if (invoiceTotal >= 250 && invoiceTotal < 500) {
+        discountPercent = 0.2;  
+    } else {
+        discountPercent = 0.25; 
+    }
+    
+}else if (customerType === "w") {
+    
+    if (invoiceTotal > 0 && invoiceTotal < 500) {
+        discountPercent = 0.4;
+    } else {
+        discountPercent = 0.5; 
+    }
+} else {
+    discountPercent = 0;
+}
+discountAmount = invoiceTotal * discountPercent;
+newInvoiceTotal = invoiceTotal - discountAmount;
+
+window.document.write("Invoice total: $" + invoiceTotal + "<br>");
+window.document.write("Discount percent: $" + discountPercent * 100 + "%<br>");
+window.document.write("Discount amount: $" + discountAmount + "<br>");
+window.document.write("New Invoice Total: $" + newInvoiceTotal + "<br>");
